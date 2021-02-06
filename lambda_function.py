edc6a14ef8854eed07d5b671434b04d631f6458a
DimonLuk <dimonluk2.0@gmail.com>
2021-02-06 02:40:01 +0200

    Add illustrative subsegment with metada, annotation and exception

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 import boto3
-from aws_xray_sdk.core import patch_all
+from aws_xray_sdk.core import patch_all, xray_recorder
 
 patch_all()
 
@@ -12,6 +12,10 @@ BUCKET_NAME = "xrayinvestigation"
 def lambda_handler(event, context):
     raw_data = S3.get_object(Bucket=BUCKET_NAME, Key="sample_data.json")
     data = json.loads(raw_data["Body"].read())
+    with xray_recorder.in_subsegment("Throwing exception") as subsegment:
+        subsegment.put_metadata("very_important_inforamtion", {"message": "Hello I am metadata"}, "information")
+        subsegment.put_annotation("very_important_annotation", "Very very important information")
+        raise Exception("Information")
     return {
         "statusCode": 200,
         "body": data["message"]
